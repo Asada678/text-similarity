141302bd33ace432f781d710a895ef181df11ae0
Asada678 <asada.tomoya0923@gmail.com>
2023-03-21 21:41:37 +0900

    add revoke api key

---
+++ src/components/ApiKeyOptions.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { DropdownMenu } from "@/ui/DropdownMenu";
+
+interface ApiKeyOptionsProps {}
+
+const ApiKeyOptions: FC<ApiKeyOptionsProps> = ({}) => {
+  return <DropdownMenu>
+    <DropdownMenuTrigger
+    ></DropdownMenuTrigger>
+  </DropdownMenu>
+};
+
+export default ApiKeyOptions;
